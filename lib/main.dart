26953c1291ffdb6508b9c2660bd665568851c0df
mac <macalbus.ma@gmail.com>
2025-05-20 10:26:24 -0600

    added firebase auth and dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 import 'app.dart';
 
 Future<void> main() async {
